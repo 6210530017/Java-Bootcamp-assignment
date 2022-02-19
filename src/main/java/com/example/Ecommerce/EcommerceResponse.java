31ea9f191da85096038d08c213749913fe7fdf0f
Pramote Siriparp <6210530017@student.tu.ac.th>
2022-02-19 16:07:27 +0700

    change pattern of response

--- src/main/java/com/example/Ecommerce/EcommerceResponse.java
+++ src/main/java/com/example/Ecommerce/EcommerceResponse.java
@@ -1,18 +1,69 @@
 package com.example.Ecommerce;
 
+import java.util.List;
+import java.util.Optional;
+
 public class EcommerceResponse {
 
-    private String message;
+//    private String message;
+//
+//    public EcommerceResponse(String message) {
+//        this.message = message;
+//    }
+//
+//    public String getMessage() {
+//        return message;
+//    }
+//
+//    public void setMessage(String message) {
+//        this.message = message;
+//    }
 
-    public EcommerceResponse(String message) {
-        this.message = message;
+    private List<Product> products;
+    private User user;
+    private List<Product> basket;
+    private PaymentMethod  paymentMethod;
+
+    public EcommerceResponse(List<Product> products, User user, List<Product> basket, PaymentMethod paymentMethod) {
+        this.products = products;
+        this.user = user;
+        this.basket = basket;
+        this.paymentMethod = paymentMethod;
     }
 
-    public String getMessage() {
-        return message;
+    public EcommerceResponse(List<Product> products) {
+        this.products = products;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Product> getBasket() {
+        return basket;
+    }
+
+    public void setBasket(List<Product> basket) {
+        this.basket = basket;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
     }
 }
